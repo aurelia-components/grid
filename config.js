48ec5270541ee7f63ab2ca3a91045d47fd55201a
Tsvetina Georgieva <tsvetina.georgieva@scalefocus.com>
2016-04-27 17:15:47 +0300

    moment dependency added

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ System.config({
 
   map: {
     "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.0",
+    "moment": "npm:moment@2.13.0",
     "npm:aurelia-polyfills@1.0.0-beta.1.1.0": {
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0"
     }
